01756801fff398b54ef2353c7609ad9bf82de544
takakikasuga <takaki.soccer1@gmail.com>
2022-06-12 11:37:38 +0900

    :sparkles: GraphQLデザインパターンPJのセットアップ

---
+++ initial-project/index.js
@@ -0,0 +1,54 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    cars: [Car!]!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+
+  type ManualGroup {
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+    memberships: [GroupMembership!]!
+  }
+
+  type AutomaticGroup {
+    id: ID!
+    name: String!
+    imageId: ID!
+    bodyHtml: String!
+    memberships: [GroupMembership!]!
+    features: [AutomaticGroupFeature!]!
+    applyFeaturesSeparately: Boolean!
+  }
+
+  type AutomaticGroupFeature {
+    column: String!
+  }
+
+  # NOTE: Many to Many Relationships
+  type GroupMembership {
+    groupId: ID!
+    carId: ID!
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      cars: () => [{ id: 1, color: 'blue', make: 'Toyota' }]
+    }
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
