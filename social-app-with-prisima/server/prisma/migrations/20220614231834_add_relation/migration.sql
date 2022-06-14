9391fe84dfb519239727d6938505bb90c8106a6c
takakikasuga <takaki.soccer1@gmail.com>
2022-06-15 08:20:56 +0900

    :sparkles: 外部キー制約を追加

---
+++ social-app-with-prisima/server/prisma/migrations/20220614231834_add_relation/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Bio` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[authorId]` on the table `Post` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "authorId" INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE "Bio";
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_authorId_key" ON "Post"("authorId");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
