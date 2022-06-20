aed08a6b74454941467d457f7b58f436c31f63c7
takakikasuga <takaki.soccer1@gmail.com>
2022-06-21 07:55:05 +0900

    :sparkles: 投稿データの全件取得

--- social-app-with-prisima/server/src/resolvers/Query.ts
+++ social-app-with-prisima/server/src/resolvers/Query.ts
@@ -1,3 +1,15 @@
+import type { Context } from '../types';
+
 export const Query = {
-  hello: () => "Hello world!",
+  posts: async (_: any, __: any, { prisma }: Context) => {
+    const posts = await prisma.post.findMany({
+      orderBy: [
+        {
+          createdAt: 'desc'
+        }
+      ]
+    });
+
+    return posts;
+  }
 };
