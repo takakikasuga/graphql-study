3ec7542b62e496caa4974519c785dd50e66f27b2
takakikasuga <takaki.soccer1@gmail.com>
2022-06-26 16:32:57 +0900

    :sparkles: 動的な変数をもとにクエリする実装対応

--- social-app-with-prisima/client/src/pages/Profile/Profile.js
+++ social-app-with-prisima/client/src/pages/Profile/Profile.js
@@ -1,27 +1,69 @@
-import React from "react";
-import { useParams } from "react-router";
-import AddPostModal from "../../components/AddPostModal/AddPostModal";
-import Post from "../../components/Post/Post";
+import React from 'react';
+import { useParams } from 'react-router';
+import AddPostModal from '../../components/AddPostModal/AddPostModal';
+import Post from '../../components/Post/Post';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_PROFILE = gql`
+  query GetProfile($userId: ID!) {
+    profile(userId: $userId) {
+      id
+      bio
+      user {
+        id
+        name
+        posts {
+          id
+          title
+          content
+          createdAt
+          published
+        }
+      }
+    }
+  }
+`;
 
 export default function Profile() {
   const { id } = useParams();
+  const { data, error, loading } = useQuery(GET_PROFILE, {
+    variables: {
+      userId: id
+    }
+  });
+  console.log({ data, error, loading });
+  if (error) return <div>Error Page</div>;
+  if (loading) return <div>Spinner...</div>;
+  const { profile } = data;
 
   return (
     <div>
       <div
         style={{
-          marginBottom: "2rem",
-          display: "flex ",
-          justifyContent: "space-between",
-        }}
-      >
+          marginBottom: '2rem',
+          display: 'flex ',
+          justifyContent: 'space-between'
+        }}>
         <div>
-          <h1>Profile Name</h1>
-          <p>Profile Bio</p>
+          <h1>{profile.user.name}</h1>
+          <p>{profile.bio}</p>
         </div>
-        <div>{"profile" ? <AddPostModal /> : null}</div>
+        <div>{profile ? <AddPostModal /> : null}</div>
       </div>
-      <div></div>
+      <div>
+        {profile.user.posts.map(({ title, content, createdAt, id, user }) => {
+          return (
+            <Post
+              key={id}
+              id={id}
+              title={title}
+              content={content}
+              date={createdAt}
+              user={profile.user.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
