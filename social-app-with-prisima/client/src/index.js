df1971a07e49da28fefe956cc19043fd8f4ca5e7
takakikasuga <takaki.soccer1@gmail.com>
2022-06-26 17:00:10 +0900

    :sparkles: トークンを使った通信を確率

--- social-app-with-prisima/client/src/index.js
+++ social-app-with-prisima/client/src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token
+    }
+  };
+});
 
 const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 });
