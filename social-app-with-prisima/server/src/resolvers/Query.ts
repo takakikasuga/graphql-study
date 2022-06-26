af6322e285be8811fa0d28150cdec9186064aff6
takakikasuga <takaki.soccer1@gmail.com>
2022-06-26 11:54:56 +0900

    :sparkles: 自分のプロフィールを参照できるように実装

--- social-app-with-prisima/server/src/resolvers/Query.ts
+++ social-app-with-prisima/server/src/resolvers/Query.ts
@@ -1,6 +1,17 @@
 import type { Context } from '../types';
 
 export const Query = {
+  me: async (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) {
+      return null;
+    }
+
+    return await prisma.user.findUnique({
+      where: {
+        id: userInfo.userId
+      }
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     const posts = await prisma.post.findMany({
       orderBy: [
